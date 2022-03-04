aaed7315a1bb93ac61e0770dc0d5a63b15ddba47
Freddie <frice@digitalocean.com>
2022-03-04 14:53:22 -0600

    add app-cmd

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/freddierice/app-cmd
+
+go 1.13
+
+require github.com/gofrs/uuid v3.3.0+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gofrs/uuid"
+)
+
+func logRequest(r *http.Request) {
+	uri := r.RequestURI
+	method := r.Method
+	fmt.Println("Got request!", method, uri)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		logRequest(r)
+		fmt.Fprintf(w, "Hello! you've requested %s\n", r.URL.Path)
+	})
+
+	http.HandleFunc("/cached", func(w http.ResponseWriter, r *http.Request) {
+		maxAgeParams, ok := r.URL.Query()["max-age"]
+		if ok && len(maxAgeParams) > 0 {
+			maxAge, _ := strconv.Atoi(maxAgeParams[0])
+			w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%d", maxAge))
+		}
+		responseHeaderParams, ok := r.URL.Query()["headers"]
+		if ok {
+			for _, header := range responseHeaderParams {
+				h := strings.Split(header, ":")
+				w.Header().Set(h[0], strings.TrimSpace(h[1]))
+			}
+		}
+		statusCodeParams, ok := r.URL.Query()["status"]
+		if ok {
+			statusCode, _ := strconv.Atoi(statusCodeParams[0])
+			w.WriteHeader(statusCode)
+		}
+		requestID := uuid.Must(uuid.NewV4())
+		fmt.Fprintf(w, requestID.String())
+	})
+
+	http.HandleFunc("/headers", func(w http.ResponseWriter, r *http.Request) {
+		keys, ok := r.URL.Query()["key"]
+		if ok && len(keys) > 0 {
+			fmt.Fprintf(w, r.Header.Get(keys[0]))
+			return
+		}
+		headers := []string{}
+		for key, values := range r.Header {
+			headers = append(headers, fmt.Sprintf("%s=%s", key, strings.Join(values, ",")))
+		}
+		fmt.Fprintf(w, strings.Join(headers, "\n"))
+	})
+
+	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
+		keys, ok := r.URL.Query()["key"]
+		if ok && len(keys) > 0 {
+			fmt.Fprintf(w, os.Getenv(keys[0]))
+			return
+		}
+		envs := []string{}
+		for _, env := range os.Environ() {
+			envs = append(envs, env)
+		}
+		fmt.Fprintf(w, strings.Join(envs, "\n"))
+	})
+
+	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		listParams, ok := r.URL.Query()["list"]
+		if ok && len(listParams) > 0 {
+			files, err := ioutil.ReadDir(listParams[0])
+			if err != nil {
+				fmt.Fprintf(w, "error: %v", err)
+				return
+			}
+
+			for _, f := range files {
+				fmt.Fprintf(w, "%v\n", f.Name())
+			}
+		}
+	})
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		codeParams, ok := r.URL.Query()["code"]
+		if ok && len(codeParams) > 0 {
+			statusCode, _ := strconv.Atoi(codeParams[0])
+			if statusCode >= 200 && statusCode < 600 {
+				w.WriteHeader(statusCode)
+			}
+		}
+		requestID := uuid.Must(uuid.NewV4())
+		fmt.Fprintf(w, requestID.String())
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	for _, encodedRoute := range strings.Split(os.Getenv("ROUTES"), ",") {
+		if encodedRoute == "" {
+			continue
+		}
+		pathAndBody := strings.SplitN(encodedRoute, "=", 2)
+		path, body := pathAndBody[0], pathAndBody[1]
+		http.HandleFunc("/"+path, func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, body)
+		})
+	}
+
+	bindAddr := fmt.Sprintf(":%s", port)
+	fmt.Printf("==> Server listening at %s 🚀\n", bindAddr)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	if err != nil {
+		panic(err)
+	}
+}
